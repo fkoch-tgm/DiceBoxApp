ee3254892b698c7540c45868fa3ce0a409f8c549
Felix Koch <fkoch@student.tgm.ac.at>
2020-11-11 16:43:44 +0100

    add Disadvantage

---
+++ app/src/main/java/at/ac/tgm/fkoch/dicebox/model/DisadvantageRoll.java
@@ -0,0 +1,34 @@
+package at.ac.tgm.fkoch.dicebox.model;
+
+
+/**
+ * Symbolisiert einen Würfelwurf mit "Advantage", wobei zwei Würfel geworfen werden und der höhere Wurf zählt
+ */
+public class DisadvantageRoll extends RollDecoration {
+
+	private DiceRoll secondDice;
+
+	public DisadvantageRoll(DiceRoll roll, DiceRoll secondRoll) {
+		super(roll);
+		this.secondDice = secondRoll;
+	}
+
+	@Override
+	public void roll() {
+		super.roll();
+		secondDice.roll();
+	}
+
+	@Override
+	public String toString() {
+		return " Disadvantage["+super.getResult()+", "+secondDice.getResult()+"]";
+	}
+
+	@Override
+	public int getResult() {
+		int r1 = super.getResult();
+		int r2 = secondDice.getResult();
+		return Math.min(r1, r2);
+	}
+
+}
