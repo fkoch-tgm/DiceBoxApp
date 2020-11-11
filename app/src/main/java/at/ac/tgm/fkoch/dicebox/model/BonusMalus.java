959062c8271363f82774afced55e4841a96dcfb9
Felix Koch <fkoch@student.tgm.ac.at>
2020-11-11 16:58:41 +0100

    upload templates

---
+++ app/src/main/java/at/ac/tgm/fkoch/dicebox/model/BonusMalus.java
@@ -0,0 +1,26 @@
+package at.ac.tgm.fkoch.dicebox.model;
+
+//TODO
+public class BonusMalus extends RollDecoration {
+
+	private String type;
+
+	private int amount;
+
+	public BonusMalus(DiceRoll roll, String type, int amount) {
+
+	}
+
+	public void roll() {
+
+	}
+
+	public String toString() {
+		return null;
+	}
+
+	public int getResult() {
+		return 0;
+	}
+
+}
---
+++ app/src/main/java/at/ac/tgm/fkoch/dicebox/model/CriticalSuccess.java
@@ -0,0 +1,24 @@
+package at.ac.tgm.fkoch.dicebox.model;
+
+/**
+ * einen Kritischen TODO
+ */
+public class CriticalSuccess extends RollDecoration {
+
+	public CriticalSuccess(DiceRoll roll) {
+
+	}
+
+	public void roll() {
+
+	}
+
+	public String toString() {
+		return null;
+	}
+
+	public int getResult() {
+		return 0;
+	}
+
+}
