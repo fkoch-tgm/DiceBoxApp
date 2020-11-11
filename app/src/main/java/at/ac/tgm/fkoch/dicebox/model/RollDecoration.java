fbf84f51c501054a50b4dc6a7cd7f0d59673d8eb
Felix Koch <fkoch@student.tgm.ac.at>
2020-11-11 16:33:55 +0100

    RollDecoration

---
+++ app/src/main/java/at/ac/tgm/fkoch/dicebox/model/RollDecoration.java
@@ -0,0 +1,43 @@
+package at.ac.tgm.fkoch.dicebox.model;
+
+/**
+ * Symbolisiert die Gemeinsamkeiten aller dekorierenden DiceRoll-Methoden
+ * @author Felix Koch
+ * @version 2020-11-11
+ */
+public abstract class RollDecoration implements DiceRoll {
+
+	private DiceRoll sourceRoll;
+
+	/**
+	 * Erstellt eine neue Würfelwurf-Dekoration
+	 * @param sourceRoll der zugrundeliegende Würfelwurf
+	 */
+	RollDecoration(DiceRoll sourceRoll){
+		this.sourceRoll = sourceRoll;
+	}
+
+	/**
+	 * @return den zugrundeliegenden Würfelwurf
+	 */
+	public DiceRoll getRoll() {
+		return sourceRoll;
+	}
+
+	@Override
+	public String toString() {
+		return sourceRoll.toString();
+	}
+
+	@Override
+	public int getResult() {
+		return sourceRoll.getResult();
+	}
+
+
+	@Override
+	public void roll() {
+		sourceRoll.roll();
+	}
+
+}
