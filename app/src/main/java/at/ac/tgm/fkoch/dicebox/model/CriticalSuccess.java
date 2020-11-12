b8b38fa07b0770f53547c900a1235cd2d9cb4ef5
Felix Koch <fkoch@student.tgm.ac.at>
2020-11-12 21:00:27 +0100

    crit

--- app/src/main/java/at/ac/tgm/fkoch/dicebox/model/CriticalSuccess.java
+++ app/src/main/java/at/ac/tgm/fkoch/dicebox/model/CriticalSuccess.java
@@ -1,24 +1,27 @@
 package at.ac.tgm.fkoch.dicebox.model;
 
 /**
- * einen Kritischen TODO
+ * Ein kritscher-Erfolgswurf hat den doppelten Wert des eigentlichen Würfelwerts
+ * @author Felix Koch
+ * @version 2020-11-12
  */
 public class CriticalSuccess extends RollDecoration {
 
+	/**
+	 * Erstellt einen neune Würfel, wo jeder Wert kritisch ist
+	 * @param roll der Basiswürfel
+	 */
 	public CriticalSuccess(DiceRoll roll) {
-
-	}
-
-	public void roll() {
-
+		super(roll);
 	}
 
 	public String toString() {
-		return null;
+		return "Kritisch("+getResult()+")";
 	}
 
+
 	public int getResult() {
-		return 0;
+		return super.getResult()*2;
 	}
 
 }
