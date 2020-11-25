8a43159bb5d7092f59946c65d5422eb43b6ff193
Felix Koch <fkoch@student.tgm.ac.at>
2020-11-25 12:43:04 +0100

    implement bonis

--- app/src/main/java/at/ac/tgm/fkoch/dicebox/MainActivity.java
+++ app/src/main/java/at/ac/tgm/fkoch/dicebox/MainActivity.java
@@ -1,15 +1,21 @@
 package at.ac.tgm.fkoch.dicebox;
 
 import android.os.Bundle;
-import android.widget.TextView;
-import at.ac.tgm.fkoch.dicebox.model.BasicDiceRoll;
-import at.ac.tgm.fkoch.dicebox.model.DiceRoll;
+import android.widget.*;
+import at.ac.tgm.fkoch.dicebox.model.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private TextView display;
     private TextView description;
+    private CheckBox abilityCheck;
+    private CheckBox profCheck;
+    private CheckBox itemCheck;
+    private EditText abilityBonus;
+    private EditText profBonus;
+    private EditText itemBonus;
+    private RadioGroup advGrp;
     private DiceRoll die;
 
     @Override
@@ -23,6 +29,20 @@ public class MainActivity extends AppCompatActivity {
         // set-up views
         display = findViewById(R.id.resultDisplay);
         description = findViewById(R.id.descriptionText);
+
+        advGrp = findViewById(R.id.advantageGroup);
+        advGrp.setOnCheckedChangeListener((btnGrp,checkedID)->updateDie());
+
+        abilityCheck = findViewById(R.id.checkAbility);
+        abilityCheck.setOnCheckedChangeListener((buttonView, isChecked) -> updateDie());
+        profCheck = findViewById(R.id.checkProficiency);
+        profCheck.setOnCheckedChangeListener((a,b)-> updateDie());
+        itemCheck = findViewById(R.id.checkItem);
+        itemCheck.setOnCheckedChangeListener((a,b)-> updateDie());
+
+        abilityBonus = findViewById(R.id.abilityBonus);
+        profBonus = findViewById(R.id.profBonus);
+        itemBonus = findViewById(R.id.itemBonus);
     }
 
     /**
@@ -34,5 +54,36 @@ public class MainActivity extends AppCompatActivity {
         description.setText(die.toString());
     }
 
+    private void updateDie() {
+        // select advantage
+        switch (advGrp.getCheckedRadioButtonId()) {
+            case R.id.advantageBtn:
+                die = new AdvantageRoll(new BasicDiceRoll(20), new BasicDiceRoll(20));
+                break;
+            case R.id.disadvantageBtn:
+                die = new DisadvantageRoll(new BasicDiceRoll(20),new BasicDiceRoll(20));
+                break;
+            case R.id.normalBtn:
+            default:
+                die = new BasicDiceRoll(20);
+        }
 
+        // Bonis Ability
+        if(abilityCheck.isChecked()) {
+            String txt = abilityBonus.getText().toString();
+            int amount = txt.equals("")?0:Integer.parseInt(txt);
+            die = new BonusMalus(die, getText(R.string.abilityText).toString(), amount);
+        }
+        if(itemCheck.isChecked()) {
+            String txt = itemBonus.getText().toString();
+            int amount = txt.equals("")?0:Integer.parseInt(txt);
+            die = new BonusMalus(die, getText(R.string.itemText).toString(), amount);
+        }
+        if(profCheck.isChecked()) {
+            String txt = profBonus.getText().toString();
+            int amount = txt.equals("")?0:Integer.parseInt(txt);
+            die = new BonusMalus(die, getText(R.string.profText).toString(), amount);
+        }
+
+    }
 }
