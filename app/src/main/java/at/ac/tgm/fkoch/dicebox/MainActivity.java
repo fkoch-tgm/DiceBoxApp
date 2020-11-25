1fc8c8d0028aff0114982ab56350244e43406c28
Felix Koch <fkoch@student.tgm.ac.at>
2020-11-25 10:55:52 +0100

    implement default roll

--- app/src/main/java/at/ac/tgm/fkoch/dicebox/MainActivity.java
+++ app/src/main/java/at/ac/tgm/fkoch/dicebox/MainActivity.java
@@ -1,21 +1,37 @@
 package at.ac.tgm.fkoch.dicebox;
 
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.TextView;
+import at.ac.tgm.fkoch.dicebox.model.BasicDiceRoll;
+import at.ac.tgm.fkoch.dicebox.model.DiceRoll;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity {
+    private TextView display;
+    private TextView description;
+    private DiceRoll die;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set-Up a Basic Dice Roll
+        die = new BasicDiceRoll(20);
+
+        // set-up views
+        display = findViewById(R.id.resultDisplay);
+        description = findViewById(R.id.descriptionText);
+    }
+
+    /**
+     * Rolls the die
+     */
+    public void rollDie(View view) {
+        die.roll();
+        display.setText(String.valueOf(die.getResult()));
+        description.setText(die.toString());
     }
 
 
